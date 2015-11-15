7b7de9654681c021268bb6d27c937de7ba76c681
JOAO LUCAS RULFF DA COSTA <joaolucasrulffdacosta@JOAOs-MacBook-Pro.local>
2015-11-15 12:27:16 -0500

    Collision done

---
+++ android/src/us/com/joao/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package us.com.joao.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import us.com.joao.game.CollisionDemo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CollisionDemo(), config);
+	}
+}
---
+++ desktop/src/us/com/joao/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package us.com.joao.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import us.com.joao.game.CollisionDemo;
+import us.com.joao.game.Physics4;
+
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Physics4(), config);
+	}
+}
